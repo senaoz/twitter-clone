80c4044cb3d28a7c4d14c958c718980126bfb200
Sena Oz <tazeyta@gmail.com>
2022-08-10 21:37:00 +0300

    10 August

--- server/db.js
+++ server/db.js
@@ -1,11 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "prhhahmhvjkokk",
-  host: "ec2-54-229-217-195.eu-west-1.compute.amazonaws.com",
-  database: "d5eeggoe7oo4pr",
-  password: "dba29aff001bcc85b31c50e9a1fa43d76f9b39ccf78149fca4a3806bda706198",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = "*";
+const port = "3000";
 const pool = require("./db");
 const cors = require("cors");
 var bodyParser = require("body-parser");
